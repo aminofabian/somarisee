bf3923d99166321662c95cf32eff2fc445a8847d
Fabian Amino <aminofab@gmail.com>
2024-11-07 19:41:44 +0300

    added

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>Here's where you log in</div>
+  )
+}
+
+export default LoginPage;
---
+++ src/app/(front)/page.tsx
@@ -0,0 +1,12 @@
+import SiteHeader from '@/components/site-head';
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+    <SiteHeader />
+    </div>
+  )
+}
+
+export default HomePage;
